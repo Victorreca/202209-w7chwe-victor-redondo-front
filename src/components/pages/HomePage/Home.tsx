7f1805c74ab0ba2831994d7f1399933d595637b6
Victor Redondo <vrviktor@gmail.com>
2022-11-14 19:22:48 +0100

    Edit sonar workflow

--- src/components/pages/HomePage/Home.tsx
+++ src/components/pages/HomePage/Home.tsx
@@ -1,12 +1,7 @@
 const Home = (): JSX.Element => {
   return (
     <>
-      <div className="page-header">
-        <span className="page-header__subtitle">
-          In search of new friends or enemies
-        </span>
-        <span className="page-header__title">Discover new people</span>
-      </div>
+      <div className="page-header"></div>
     </>
   );
 };
