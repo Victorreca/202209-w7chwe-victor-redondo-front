55f0bd1c24556242092b8e007eaf8d812c24ccb0
Victor Redondo <vrviktor@gmail.com>
2022-11-16 21:13:41 +0100

    Add routes in App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import RegisterPage from "../pages/RegisterPage/RegisterPage";
+import RegisterPage from "../../pages/RegisterPage/RegisterPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
 
 const App = () => {
   return (
     <div className="app">
       <Routes>
-        <Route path="/home" element={<RegisterPage />} />
+        <Route path="/home" element={<LoginPage />} />
         <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
